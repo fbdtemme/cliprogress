51923f70ec51a9e41bc1765e5eecba06aace8480
fbdtemme <floriandetemmerman@gmail.com>
2021-01-14 17:04:33 +0100

    Fix fetching dependencies

--- external/external.cmake
+++ external/external.cmake
@@ -10,6 +10,22 @@ function(log_not_found library)
     message(STATUS "Fetching dependency ${library}...")
 endfunction()
 
+
+find_package(fmt QUIET)
+if (fmt_FOUND)
+    log_found(fmt)
+else()
+    log_not_found(fmt)
+    FetchContent_Declare(
+            fmt
+            GIT_REPOSITORY https://github.com/fmtlib/fmt.git
+            GIT_TAG        master
+    )
+    set(FORMAT_BUILD_TESTING OFF)
+    set(FMT_INSTALL ON)
+    FetchContent_MakeAvailable(fmt)
+endif()
+
 find_package(PalSigslot  QUIET)
 if (PalSigslot_FOUND)
     log_found(PalSigslot)
@@ -46,7 +62,21 @@ else()
     FetchContent_Declare(
             termcontrol
             GIT_REPOSITORY https://github.com/fbdtemme/termcontrol.git
-            GIT_TAG        master
+            GIT_TAG        main
     )
     FetchContent_MakeAvailable(termcontrol)
+endif()
+
+find_package(Catch2 QUIET)
+if (Catch2_FOUND)
+    log_found(Catch2)
+else()
+    log_not_found(Catch2)
+    FetchContent_Declare(
+            Catch2
+            GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+            GIT_TAG        v2.x
+    )
+    FetchContent_MakeAvailable(Catch2)
+    set(CMAKE_MODULE_PATH "${Catch2_SOURCE_DIR}/contrib" ${CMAKE_MODULE_PATH})
 endif()
